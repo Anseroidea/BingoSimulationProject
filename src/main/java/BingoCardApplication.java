8a801db16d45eea03467f586bc64737a66fd1283
Anseroidea <K1525058@students.katyisd.rog>
2021-08-30 08:05:41 -0500

    updates

--- src/main/java/BingoCardApplication.java
+++ src/main/java/BingoCardApplication.java
@@ -1,20 +1,22 @@
+import com.sun.tools.javac.Main;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class BingoCardApplication extends Application {
 
-    public static final int WIDTH = 400;
-    public static final int HEIGHT = 500;
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 400;
 
     @Override
     public void start(Stage primaryStage){
-        System.out.println((byte) -98);
         primaryStage.setTitle("Bingo Card");
         primaryStage.setResizable(false);
         BingoCardHandler bch = new BingoCardHandler(100);
         BingoCardLayout.displayBingoCard(bch.generateNewBingoCard());
-        primaryStage.setScene(new Scene(BingoCardLayout.getAnchorPane(), WIDTH, HEIGHT));
+        primaryStage.setScene(MainMenuScene.getMainMenu());
         primaryStage.show();
     }
 
--- src/main/java/MainMenuScene.java
+++ src/main/java/MainMenuScene.java
@@ -1,13 +1,30 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
 public class MainMenuScene {
 
+    private static Scene mainMenu;
+    @FXML private static AnchorPane anchorPane;
+
     static {
-
-
-
+        try {
+            FXMLLoader loader = FXMLLoader.load(MainMenuScene.class.getResource("/fxml/MainMenuScene"));
+            anchorPane = loader.load();
+            mainMenu = new Scene(anchorPane);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
+    public static void doMainMenuButton(){
+        System.out.println("Hey");
+    }
 
+    public static Scene getMainMenu(){
+        return mainMenu;
+    }
 
 }
