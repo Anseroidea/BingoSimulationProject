69d9bf4f27b61a423ccb38b8c990fd7e989cc13c
Anseroidea <K1525058@students.katyisd.rog>
2021-09-10 08:05:45 -0500

    updates

--- src/main/java/sim/BingoSimulationLayout.java
+++ src/main/java/sim/BingoSimulationLayout.java
@@ -1,11 +1,12 @@
 package sim;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.Property;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.MapChangeListener;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -15,6 +16,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 import jdk.jshell.spi.SPIResolutionException;
 import ui.*;
 import card.*;
@@ -24,11 +26,10 @@ import java.util.*;
 
 public class BingoSimulationLayout implements Initializable{
 
-    public TableView<Map.Entry<Integer, Integer>> rollsTable;
-    @FXML private TableColumn<String, Integer> ballRolledColumn;
-    @FXML private TableColumn<String, Integer> bingoIDColumn;
-    @FXML private TableColumn<String, Integer> rollColumn2;
-    @FXML private TableColumn<Map.Entry<Integer, Integer>, Integer> rollColumn;
+    @FXML private TableView<Integer> bingoWinnerTable;
+    @FXML private TableView<Integer> rollsTable;
+    private ObservableMap<Integer, Integer> map;
+    private ObservableList<Integer> keys;
     private int selectedCard;
     private int numWinners;
     @FXML private Label ballLabel;
@@ -39,8 +40,10 @@ public class BingoSimulationLayout implements Initializable{
 
 
     public void rollBallFunction(MouseEvent mouseEvent) {
-        BingoCardApplication.getSimulation().nextRoll();
+        BingoBall ball = BingoCardApplication.getSimulation().nextRoll();
         rollLabel.setText("Roll " + BingoCardApplication.getSimulation().getBallsRolled());
+        map.put(BingoCardApplication.getSimulation().getBallsRolled(), ball.getI());
+        rollsTable.setItems(keys);
         BingoCardLayout.displayBingoCard();
         BingoCardApplication.refreshCurrentScene();
     }
@@ -63,10 +66,13 @@ public class BingoSimulationLayout implements Initializable{
     public void rollNWinnersFunction(MouseEvent mouseEvent) {
         if (BingoCardApplication.getSimulation().getBingoCardHandler().getRemainingCards().size() == 0)
             return;
+        BingoBall b = null;
         while (BingoCardApplication.getSimulation().getBingoCardHandler().getWonCards().size() < numWinners){
-            BingoCardApplication.getSimulation().nextRoll();
+            b = BingoCardApplication.getSimulation().nextRoll();
         }
-        rollLabel.setText("Roll " + BingoCardApplication.getSimulation().getBallsRolled());
+        if (b == null)
+            return;
+        rollLabel.setText("Roll " + BingoCardApplication.getSimulation().getBallsRolled() + ": " + b.getI());
         BingoCardLayout.displayBingoCard();
         BingoCardApplication.refreshCurrentScene();
     }
@@ -101,57 +107,28 @@ public class BingoSimulationLayout implements Initializable{
         rollNWinnersButton.setText(label);
     }
 
-
-    public void setCardDisplayed(InputMethodEvent inputMethodEvent) {
-        System.out.println(idSpinner);
-    }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ballRolledColumn.getTableView().setEditable(false);
-        bingoIDColumn.getTableView().setEditable(false);
-        Map<Integer, Integer> test = new HashMap<>();
-
-        ObservableList<Map.Entry<Integer, Integer>> list = FXCollections.observableList(Arrays.asList(
-        ));
-        rollColumn.setCellValueFactory(
-                new PropertyValueFactory<>("value")
-        );
-        rollColumn.getTableView().setItems(list);
+        map = FXCollections.observableHashMap();
+        keys = FXCollections.observableArrayList();
+        rollsTable = new TableView<>(keys);
+        map.addListener((MapChangeListener.Change<? extends Integer, ? extends Integer> change) -> {
+            boolean removed = change.wasRemoved();
+            if (removed != change.wasAdded()) {
+                if (removed) {
+                    keys.remove(change.getKey());
+                } else {
+                    keys.add(change.getKey());
+                }
+            }
+        });
+        map.put(2, 4);
+        TableColumn<Integer, Integer> rollColumn = new TableColumn<>("Roll");
+        TableColumn<Integer, Integer> ballColumn = new TableColumn<>("Ball");
+        rollColumn.setCellValueFactory(cd -> Bindings.createIntegerBinding(cd::getValue).asObject());
+        ballColumn.setCellValueFactory(cd -> Bindings.valueAt(map, cd.getValue()));
+        rollsTable.getColumns().clear();
+        rollsTable.getColumns().setAll(rollColumn, ballColumn);
     }
-
-    /*
-    private static <K, V> Map.Entry<K, V> toEntry(K input, V inputValue) {
-        return new Map.Entry<>() {
-
-            private final K key = input;
-            public V value = inputValue;
-
-            @Override
-            public K getKey() {
-                return key;
-            }
-
-            @Override
-            public V getValue() {
-                return value;
-            }
-
-            @Override
-            public V setValue(V value) {
-                V prev = this.value;
-                this.value = value;
-                return prev;
-            }
-
-            public ObjectProperty valueProperty(){
-                return value;
-            }
-
-
-        };
-    }
-
-     */
 
 }
